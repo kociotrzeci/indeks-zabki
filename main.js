2dff93970cab78cc596f0425698988dcfc8a9a54
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2024-05-15 22:57:17 +0200

    init

---
+++ main.js
@@ -0,0 +1,33 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL
+  await page.goto("https://www.openstreetmap.org/");
+  await page.type(".form-control", "kępno żabka");
+  await page.evaluate((b) => b.click(".btn.btn-sm.btn-primary"));
+
+  /*
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  // Type into search box
+  await page.type(".devsite-search-field", "automate beyond recorder");
+
+  // Wait and click on first result
+  const searchResultSelector = ".devsite-result-item-link";
+  await page.waitForSelector(searchResultSelector);
+  await page.click(searchResultSelector);
+
+  // Locate the full title with a unique string
+  const textSelector = await page.waitForSelector(
+    "text/Customize and automate"
+  );
+  const fullTitle = await textSelector?.evaluate((el) => el.textContent);
+
+  // Print the full title
+  console.log('The title of this blog post is "%s".', fullTitle);
+  */
+})();
