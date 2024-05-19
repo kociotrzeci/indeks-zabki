455ad8b7cbf36a604aae6083ba9cac89b6743b8c
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2024-05-19 23:07:05 +0200

    scraping OSM works

--- main.js
+++ main.js
@@ -7,12 +7,45 @@ const puppeteer = require("puppeteer");
 
   // Navigate the page to a URL
   await page.goto("https://www.openstreetmap.org/");
-  await page.type(".form-control", "kępno żabka");
-  await page.evaluate((b) => b.click(".btn.btn-sm.btn-primary"));
+  await page.waitForSelector(".form-control");
+  await page.type(".form-control", "kępno dino");
+  await page.keyboard.press("Enter");
+  await page.waitForSelector(
+    "#sidebar_content > div.search_results_entry.mx-n3 > div > a",
+    { timeout: 5000 }
+  );
+  const textContent = await page.evaluate(() => {
+    console.log(
+      document.querySelector(
+        "#sidebar_content > div.search_results_entry.mx-n3 > div > a"
+      )
+    );
+  });
+
+  let finishedScanning = 0;
+  while (!finishedScanning) {
+    await page.click(
+      "#sidebar_content > div.search_results_entry.mx-n3 > div > a"
+    );
+    try {
+      await page.waitForFunction(
+        `document.querySelector("#sidebar_content > div.search_results_entry.mx-n3 > div > a").style.display === ""`,
+        { timeout: 5000 }
+      );
+    } catch (error) {
+      console.log(error);
+      finishedScanning = 1;
+    }
+    console.log(
+      (
+        await page.$$(
+          "#sidebar_content > div.search_results_entry.mx-n3 > ul > li"
+        )
+      ).length
+    );
+  }
 
   /*
-  await page.setViewport({ width: 1080, height: 1024 });
-
   // Type into search box
   await page.type(".devsite-search-field", "automate beyond recorder");
 
@@ -31,3 +64,9 @@ const puppeteer = require("puppeteer");
   console.log('The title of this blog post is "%s".', fullTitle);
   */
 })();
+
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
